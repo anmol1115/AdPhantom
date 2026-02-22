db77f9b13374a48395261e1afd3f1d3bf3b3cd95
Anmol <anmolmoudgil@gmail.com>
2026-02-22 23:14:51 +0530

    Improved logging for tcp, added msg length limits, added timeout for slow connections

--- cmd/dns/tcp.go
+++ cmd/dns/tcp.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"net"
+	"time"
 
 	Logger "github.com/anmol1115/AdPhantom/internal/logger"
 	"github.com/miekg/dns"
@@ -18,7 +19,7 @@ func tcpListener(ctx context.Context) {
 		log.Fatal(err)
 	}
 
-	logger.Debug("Starting TCP Listener on port 53")
+	logger.Debug("TCP: Starting listener on port 53")
 	ln, err := net.Listen("tcp", ":53")
 	if err != nil {
 		logger.Error(err.Error())
@@ -28,35 +29,43 @@ func tcpListener(ctx context.Context) {
 
 	go func() {
 		<-ctx.Done()
-		logger.Debug("Closing TCP Listener")
+		logger.Debug("TCP: Closing listener")
 		ln.Close()
 	}()
 
 	for {
-		logger.Debug("Waiting for TCP connection")
+		logger.Debug("TCP: Waiting for connection")
 		conn, err := ln.Accept()
 		if err != nil {
 			if ctx.Err() != nil {
-				logger.Debug("Closing TCP Listener")
+				logger.Debug("TCP: Exiting listener loop")
 				return
 			}
 			logger.Error(err.Error())
 			continue
 		}
-		logger.Debug("TCP connection established")
 		go handleTCPConn(ctx, conn)
 	}
 }
 
 func handleTCPConn(ctx context.Context, conn net.Conn) {
-	defer conn.Close()
 	logger, err := Logger.FromContext(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	logger.Debug("TCP: Connection established from: " + conn.RemoteAddr().String())
+	defer func() {
+		logger.Debug("TCP: Connection closed from: " + conn.RemoteAddr().String())
+		conn.Close()
+	}()
+
 	lengthBytes := make([]byte, 2)
+	var dnsMsg dns.Msg
+
 	for {
+		logger.Debug("TCP: Reading byte length of request")
+		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
 		_, err := io.ReadFull(conn, lengthBytes)
 		if err != nil {
 			if errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) || errors.Is(err, net.ErrClosed) {
@@ -67,8 +76,15 @@ func handleTCPConn(ctx context.Context, conn net.Conn) {
 			return
 		}
 
+		logger.Debug("TCP: Reading request message")
 		msgLength := binary.BigEndian.Uint16(lengthBytes)
+		if msgLength == 0 || msgLength > 4096 {
+			logger.Error("TCP: Invalid length of message received")
+			return
+		}
+
 		msg := make([]byte, msgLength)
+		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
 		_, err = io.ReadFull(conn, msg)
 		if err != nil {
 			if errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) || errors.Is(err, net.ErrClosed) {
@@ -79,7 +95,8 @@ func handleTCPConn(ctx context.Context, conn net.Conn) {
 			return
 		}
 
-		var dnsMsg dns.Msg
+		logger.Debug("TCP: Parsing request")
+		dnsMsg = dns.Msg{}
 		err = dnsMsg.Unpack(msg)
 		if err != nil {
 			logger.Error(err.Error())
